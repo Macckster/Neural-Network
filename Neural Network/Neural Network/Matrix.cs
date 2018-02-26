dc5a6a31876a0eff760150215cee53e94e64ac11
Macckster <35381093+Macckster@users.noreply.github.com>
2018-02-26 20:39:42 +0100

    Update Matrix.cs

--- Neural Network/Neural Network/Matrix.cs
+++ Neural Network/Neural Network/Matrix.cs
@@ -1,8 +1,5 @@
-﻿//Created by Marcus Jansson on 2018-02-21
-//Part of neural network code
+//Created by Marcus Jansson on 2018-02-21
 //GPLV3 License
-//This document may be reproduced and reused for any purpose
-
 
 using System;
 using System.Collections;
@@ -22,20 +19,20 @@ namespace Neural_Network
 
 
         //Constructor
-        public Matrix(int rows,int cols)
+        public Matrix(int rows, int cols)
         {
             this.rows = rows;
             this.cols = cols;
 
-            matrix = new float[rows,cols];
+            matrix = new float[rows, cols];
         }
 
-        //Add two matrices togheter ir add a scalar value to alla elements in the matrix
+        //Add two matrices togheter or add a scalar value to all elements in the matrix
         public void add(object n)
         {
             Type t = n.GetType();
 
-            if(t.Equals(typeof(float)) ||t.Equals(typeof(int)))
+            if (t.Equals(typeof(float)) || t.Equals(typeof(int)))
             {
                 //Scalar add
                 for (int i = 0; i < rows; i++)
@@ -50,19 +47,46 @@ namespace Neural_Network
             {
                 //Elementwise add
 
-                float[,] tempArray = (float[,])n;
-                
+                Matrix tempArray = (Matrix)n;
+
                 for (int i = 0; i < rows; i++)
                 {
                     for (int j = 0; j < cols; j++)
                     {
-                        matrix[i, j] += tempArray[i, j];
+                        matrix[i, j] += tempArray.matrix[i, j];
                     }
                 }
             }
         }
 
+        //Convert from array to Matrix
+        internal static Matrix fromArray(object inputArray)
+        {
+            float[] input = (float[])inputArray;
 
+            Matrix m = new Matrix(input.Length, 1);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                m.matrix[i, 0] = input[i];
+            }
+            return m;
+        }
+
+        public float[] toArray()
+        {
+            float[] output = new float[cols * rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    output[i * rows + j] = matrix[i, j];
+                }
+            }
+            
+            return output;
+        }
         //Multiply two matrices togheter or multiply a scalar value to each index
         public Matrix multiply(object n)
         {
@@ -88,7 +112,7 @@ namespace Neural_Network
 
                 if (cols != mat.rows)
                 {
-                    return new Matrix(69,0);
+                    return new Matrix(69, 0);
                 }
 
                 Matrix result = new Matrix(rows, mat.cols);
@@ -126,15 +150,40 @@ namespace Neural_Network
             return result;
         }
 
-        //Generate a random value betwen 0 and 10 (inclusive) for each element in the array. These values are floats
+        //Generate a random value betwen -1 and 1 (inclusive) for each element in the array. These values are floats
         public void randomize()
         {
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] += (float)prng.NextDouble() * 10;
+                    matrix[i, j] += (float)prng.NextDouble() * 2 - 1;
                 }
+            }
+        }
+
+        //Apply a function to all elements in the matrix
+        public void map(Func<float, float> function)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = function(matrix[i, j]);
+                }
+            }
+        }
+
+        //DEBUG: PRINT ALL VALUES
+        public void print()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
             }
         }
     }
